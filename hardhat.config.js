a70559d44464fe38fdcf88b2a009329d5d7195df
Jiwon Koo <Jeewon514@gmail.com>
2026-09-10 00:49:59 +0900

    chore: set up Hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthers],
+
+  solidity: {
+    version: "0.8.20",
+  },
+});
