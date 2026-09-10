4b47ec33084b5f4a0e93fe7523713a565bc0c8c2
Jiwon Koo <Jeewon514@gmail.com>
2026-09-10 23:22:44 +0900

    feat: support dynamic validator configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,4 +7,11 @@ export default defineConfig({
   solidity: {
     version: "0.8.20",
   },
+
+  networks: {
+    localhost: {
+      type: "http",
+      url: "http://127.0.0.1:8545",
+    },
+  },
 });
