b144c2c379ffe3a82a64aba61e65ee30821f4b21
Jiwon Koo <Jeewon514@gmail.com>
2026-09-10 23:36:19 +0900

    refactor: load contract address in validator script

--- scripts/approveThreat.js
+++ scripts/approveThreat.js
@@ -4,8 +4,10 @@ import readline from "readline";
 
 const RPC_URL = "http://127.0.0.1:8545";
 
-const CONTRACT_ADDRESS =
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const deployment = JSON.parse(
+  fs.readFileSync("./deployment.json", "utf8")
+);
+const CONTRACT_ADDRESS = deployment.contractAddress;
 
 const THREAT_ID = process.argv[2];
 
